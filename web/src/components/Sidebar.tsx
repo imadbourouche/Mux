a96926abf1aff111d3dd27469dceca10c608521a
imadbourouche <imad.bourouche.pro@gmail.com>
2026-05-15 18:50:56 +0200

    Bump sidebar version label to v1.1.0

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -209,7 +209,7 @@ export function Sidebar(props: Props) {
             <JsonIcon />
           </button>
         </div>
-        <span className="sidebar-version">v1.0.0</span>
+        <span className="sidebar-version">v1.1.0</span>
       </div>
       <div className="sidebar-resizer" onMouseDown={startDrag} title="Drag to resize" />
     </aside>
